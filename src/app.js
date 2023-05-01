30ba0fc6479691d9777adf4adf042ff2c82be673
barrydillan <queharambe@gmail.com>
2023-05-01 21:43:10 +0200

    fix: fix arrow behaviour

--- src/app.js
+++ src/app.js
@@ -12,16 +12,46 @@ text.addEventListener('keydown', (event) => {
 //     // блокируем ввод символов с физической клавиатуры
 //     event.preventDefault();
 //   }
-if (event.keyCode === 38 || // arrow up
-      event.keyCode === 40 || // arrow down
-      event.keyCode === 37 || // arrow left
-      event.keyCode === 39 || // arrow right
-      event.keyCode === 8) {  // backspace
-    // Не предотвращаем действие браузера
-  } else {
-    // Предотвращаем действие браузера для всех остальных клавиш
+// if (event.keyCode === 38 || // arrow up
+//       event.keyCode === 40 || // arrow down
+//       event.keyCode === 37 || // arrow left
+//       event.keyCode === 39 || // arrow right
+//       event.keyCode === 8) {  // backspace
+//     // Не предотвращаем действие браузера
+//   } else {
+//     // Предотвращаем действие браузера для всех остальных клавиш
+//     event.preventDefault();
+//   }
+  if (event.key === 'ArrowUp') {
+    // Perform the desired action when the up arrow is pressed
+    console.log('Up arrow pressed');
+    text.value +=  '↑'
     event.preventDefault();
+
+  } else if (event.key === 'ArrowDown') {
+    // Perform the desired action when the down arrow is pressed
+    text.value +=  '↓'
+    event.preventDefault();
+
+    console.log('Down arrow pressed');
+  } else if (event.key === 'ArrowLeft') {
+    // Perform the desired action when the left arrow is pressed
+    text.value +=  '←'
+    event.preventDefault();
+
+    console.log('Left arrow pressed');
+  } else if (event.key === 'ArrowRight') {
+    // Perform the desired action when the right arrow is pressed
+    text.value +=  '→'
+    event.preventDefault();
+
+    console.log('Right arrow pressed');
+  } else {
+    event.preventDefault();
+
   }
+  
+
 });
 
 document.addEventListener('keydown', (e) => {
