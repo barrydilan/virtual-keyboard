047b181e4b4147fd57c073162298c5c2e5d21416
barrydillan <queharambe@gmail.com>
2023-05-02 03:06:33 +0200

    fix: fix eslint warnings

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
+// eslint-disable-next-line import/extensions
 import { Keyboard } from './keyboard.js';
 
 const BODY = document.querySelector('body');
 const text = document.createElement('textarea');
 text.classList.add('text');
-text.setAttribute('placeholder', 'Shift + Option (Shift + Alt on Windows). The work has been done on Mac OS.');
+text.setAttribute(
+  'placeholder',
+  'Shift + Option (Shift + Alt on Windows). The work has been done on Mac OS.',
+);
 BODY.appendChild(text);
 const userLanguage = localStorage.getItem('userLanguage') || 'en';
 const keyboard = new Keyboard(userLanguage, text);
@@ -12,27 +16,20 @@ keyboard.generateKeyboard();
 text.addEventListener('keydown', (event) => {
   if (event.key === 'ArrowUp') {
     // Perform the desired action when the up arrow is pressed
-    console.log('Up arrow pressed');
     text.value += '↑';
     event.preventDefault();
   } else if (event.key === 'ArrowDown') {
     // Perform the desired action when the down arrow is pressed
     text.value += '↓';
     event.preventDefault();
-
-    console.log('Down arrow pressed');
   } else if (event.key === 'ArrowLeft') {
     // Perform the desired action when the left arrow is pressed
     text.value += '←';
     event.preventDefault();
-
-    console.log('Left arrow pressed');
   } else if (event.key === 'ArrowRight') {
     // Perform the desired action when the right arrow is pressed
     text.value += '→';
     event.preventDefault();
-
-    console.log('Right arrow pressed');
   } else {
     event.preventDefault();
   }
